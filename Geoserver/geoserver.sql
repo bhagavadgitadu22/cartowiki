c42c8e2fc6756f637c70349830928ad15432d1be
RBoutroux <remibtrx@gmail.com>
2025-02-03 09:06:10 +0100

    [feat] Creation vues villes geoserver

--- Geoserver/geoserver.sql
+++ Geoserver/geoserver.sql
@@ -35,3 +35,39 @@ AND annee_population = (
         LIMIT 1
     );
 -- Il faut vérifier que l'annee_population est bien supérieure ou égale à %annee% et l'annee la moins éloignée de %annee% pour les autres valeurs de population du pays. C'est à ça que sert la sous-requête.
+
+
+---------- Maintenant, même chose mais pour les villes ----------
+-- Récupérer géométrie des villes pour une ville donnée
+SELECT id_entite_ville, position_ville
+FROM public.entites_ville
+WHERE id_entite_ville = %id_entite_ville%
+
+-- Récupérer populations villes pour une ville donnée
+SELECT id_entite_ville, annee, population
+FROM public.population_ville
+WHERE id_entite_ville = %id_entite_ville%
+
+-- Récupérer nom ville pour une ville donnée
+SELECT id_entite_ville, nom_ville, annee_debut, annee_fin
+FROM public.nom_ville JOIN public.ville
+ON nom_ville.id_nom_ville = ville.id_nom_ville
+WHERE id_entite_ville = %id_entite_ville%
+
+-- Récupérer tout ce qui est relatififié à une ville pour une annee donnée
+SELECT id_entite_ville, entites_ville.position_ville, population_ville.annee AS annee_population, population, nom_ville, ville.annee_debut AS annee_debut_nom, ville.annee_fin AS annee_fin_nom
+FROM public.entites_ville
+JOIN public.population_ville
+ON entites_ville.id_entite_ville = population_ville.id_entite_ville
+JOIN public.ville
+ON entites_ville.id_entite_ville = ville.id_entite_ville
+JOIN public.nom_ville
+ON nom_ville.id_nom_ville = ville.id_nom_ville
+WHERE annee_debut_nom <= %annee% AND annee_fin_nom >= %annee% AND annee_population = (
+        SELECT annee 
+        FROM public.population_ville 
+        WHERE id_entite_ville = entites_ville.id_entite_ville 
+        AND annee >= %annee% 
+        ORDER BY ABS(annee - %annee%) 
+        LIMIT 1
+    );
